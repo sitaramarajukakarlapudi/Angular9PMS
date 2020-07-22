44fda5324e1151d4e2a5b8fef8a0e0498fbd9cad
sitaramarajukakarlapudi <sitaramraj5@gmail.com>
2020-07-22 10:16:50 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MasterComponent } from './base/master/master.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'master', component: MasterComponent },
+  { path: '', redirectTo: '/master', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
