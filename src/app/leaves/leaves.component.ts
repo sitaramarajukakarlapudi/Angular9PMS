93756e9d7a6865e3618841e17112e11e219a7e48
nithinponduri <nithin.ponduri@ebix.com>
2020-07-30 15:40:06 +0530

    Leaves Changes

--- src/app/leaves/leaves.component.ts
+++ src/app/leaves/leaves.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Leave } from '../_models/leaves';
+import { SelectItem } from 'primeng/api';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-leaves',
@@ -9,92 +12,129 @@ import { Router } from '@angular/router';
 export class LeavesComponent implements OnInit {
   rangeDates: Date[];
   isValidDateRange: boolean;
+  validDates: Date[];
+  isNextClicked: boolean;
+  custF: Leave[] = [];
+  types: SelectItem[];
+  duration: SelectItem[];
+  period: SelectItem[];
+  officeTypes: SelectItem[];
+  managerTypes: SelectItem[];
+  leaveDetailsForm = new FormGroup({});
   constructor(
     private router: Router
   ) { }
 
   ngOnInit() {
     this.isValidDateRange = false;
+    this.isNextClicked = false;
+    this.types = [
+      { label: 'Select Type', value: null },
+      { label: 'Casual Leave', value: { id: 1, name: 'Casual Leave', code: 'CL' } },
+      { label: 'Compensatory', value: { id: 2, name: 'Compensatory', code: 'COMP' } },
+      { label: 'Earned Leave', value: { id: 3, name: 'Earned Leave', code: 'EL' } },
+      { label: 'Maternity', value: { id: 4, name: 'Maternity', code: 'MAT' } },
+      { label: 'On Duty', value: { id: 5, name: 'On Duty', code: 'OD' } },
+      { label: 'Paternity', value: { id: 6, name: 'Paternity', code: 'PAT' } }
+    ];
+    this.duration = [
+      { label: 'Full Day', value: { id: 1, name: 'Full Day', code: 'FD' } },
+      { label: 'Half Day', value: { id: 2, name: 'Half Day', code: 'HD' } }
+    ];
+    this.period = [
+      { label: 'Before Lunch', value: { id: 1, name: 'Before Lunch', code: 'BL' } },
+      { label: 'After Lunch', value: { id: 2, name: 'After Lunch', code: 'AL' } }
+    ];
+    this.officeTypes = [
+      { label: 'Select Location', value: null },
+      { label: 'Vijayawada', value: { id: 1, name: 'Vijayawada', code: 'VJA' } }
+    ];
+    this.managerTypes = [
+      { label: 'Select Manager', value: null },
+      { label: 'Sudheer Bijinepalli', value: { id: 1, name: 'Sudheer Bijinepalli', code: 'SB' } },
+      { label: 'Kalyan K', value: { id: 2, name: 'Kalyan K', code: 'KK' } }
+    ];
   }
 
   btnBack_Click() {
     this.router.navigate(['/menu/dashboard'], { skipLocationChange: false });
   }
 
+  btnNext_Click() {
+    this.isNextClicked = true;
+    this.custF = [];
+    for (let i = 0; i < this.validDates.length; i++) {
+      this.custF.push({
+        id: i,
+        date: this.validDates[i].toString()
+      });
+    }
+    this.addFormControls();
+  }
+
+  calShow() {
+    this.isNextClicked = false;
+  }
+
   calClose() {
     if (this.rangeDates && this.rangeDates[1]) {
       this.isValidDateRange = true;
-      // const MS_PER_DAY: number = 1000 * 60 * 60 * 24;
-      // const start: number = this.rangeDates[0].getTime();
-      // const end: number = this.rangeDates[1].getTime();
-      // const daysBetweenDates: number = Math.ceil((end - start) / MS_PER_DAY);
-
-      // // The days array will contain a Date object for each day between dates (inclusive)
-      // const days: Date[] = Array.from(new Array(daysBetweenDates + 1),
-      //   (v, i) => new Date(start + (i * MS_PER_DAY)));
-      const days = this.workingDaysBetweenDates(this.rangeDates[0], this.rangeDates[1]);
-      alert(days);
+      this.getWorkingDays(this.rangeDates[0], this.rangeDates[1]);
     } else {
       this.isValidDateRange = false;
+      this.isNextClicked = false;
     }
   }
 
-  workingDaysBetweenDates(d0, d1) {
-    const startDate: any = this.parseDate(d0);
-    const endDate: any = this.parseDate(d1);
-    const holidays = ['2020-07-28', '2020-07-25'];
-    if (endDate < startDate) {
-      return 0;
+  addFormControls() {
+    let j = 0;
+    try {
+      for (j = 0; j < this.custF.length; j++) {
+        try {
+          this.leaveDetailsForm.addControl('FCDrptype_' + this.custF[j].id, new FormControl('', null));
+          this.leaveDetailsForm.addControl('FCDrpDuration_' + this.custF[j].id, new FormControl('', null));
+          this.leaveDetailsForm.addControl('FCDrpPeriod_' + this.custF[j].id, new FormControl('', null));
+        } catch (error) {
+          console.log('Add Control' + error);
+        }
+      }
+    } catch (e) {
+      console.error('add control error ' + e.error);
     }
-    let z = 0;
-    // tslint:disable-next-line:prefer-for-of
-    for (let i = 0; i < holidays.length; i++) {
-      const cand = this.parseDate(holidays[i]);
-      const candDay = cand.getDay();
-      // if (cand >= startDate && cand <= endDate && candDay !== 0 && candDay !== 6) {
-      //   z++;
-      // }
-      if (cand >= startDate) {
-        if (cand <= endDate) {
-          if (candDay !== 0) {
-            if (candDay !== 6) {
-              z++;
+  }
+
+  getWorkingDays(startDate, endDate) {
+    this.validDates = [];
+    const MS_PER_DAY: number = 1000 * 60 * 60 * 24;
+    const start: number = this.rangeDates[0].getTime();
+    const end: number = this.rangeDates[1].getTime();
+    const daysBetweenDates: number = Math.ceil((end - start) / MS_PER_DAY);
+    let startDateNew: Date = this.parseDate(startDate);
+    const endDateNew: any = this.parseDate(endDate);
+    const holidays = ['2020-07-28', '2020-07-25'];
+    let isValidDate = true;
+    for (let i = 0; i <= daysBetweenDates; i++) {
+      isValidDate = true;
+      const finalDate = startDateNew;
+      if (startDateNew <= endDateNew) {
+        if (startDateNew.getDay() !== 0 && startDateNew.getDay() !== 6) {
+          for (const dateVal of holidays) {
+            const holidayDate = this.parseDate(dateVal);
+            if (startDateNew.toString() === holidayDate.toString()) {
+              isValidDate = false;
+              startDateNew = new Date(startDateNew.setDate(startDateNew.getDate() + 1));
+              break;
             }
           }
+          if (isValidDate) {
+            this.validDates.push(new Date(finalDate));
+            startDateNew = new Date(startDateNew.setDate(startDateNew.getDate() + 1));
+          }
+        } else {
+          startDateNew = new Date(startDateNew.setDate(startDateNew.getDate() + 1));
         }
       }
     }
-
-    const millisecondsPerDay = 86400 * 1000;
-    startDate.setHours(0, 0, 0, 1);
-    endDate.setHours(23, 59, 59, 999);
-    const diff: number = endDate - startDate;
-    let days = Math.ceil(diff / millisecondsPerDay);
-
-    const weeks = Math.floor(days / 7);
-    days = days - (weeks * 2);
-
-    // Handle special cases
-    const startDay = startDate.getDay();
-    const endDay = endDate.getDay();
-
-    // Remove weekend not previously removed.
-    if (startDay - endDay > 1) {
-      days = days - 2;
-    }
-
-    // Remove start day if span starts on Sunday but ends before Saturday
-    if (startDay === 0 && endDay !== 6) {
-      days = days - 1;
-    }
-
-    // Remove end day if span ends on Saturday but starts after Sunday
-    if (endDay === 6 && startDay !== 0) {
-      days = days - 1;
-    }
-
-    // substract the holiday dates from the original calculation and return to the DOM
-    return days - z;
   }
 
   parseDate(input) {
