27aa18dd64ecc8093eafade32d205eeb0b3c659b
nithinponduri <nithin.ponduri@ebix.com>
2020-09-02 15:55:36 +0530

    Changes

--- src/app/base/dashboard/dashboard.component.ts
+++ src/app/base/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { SelectItem } from 'primeng/api';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,10 +10,26 @@ import { Router } from '@angular/router';
 export class DashboardComponent implements OnInit {
 
   profilePath: string;
+  projects: SelectItem[];
+  selectedProject: string;
+
   constructor(private router: Router) { }
 
   ngOnInit() {
     this.profilePath = 'Profile.png';
+    this.projects = [
+      { label: 'Select Project', value: null },
+      { label: 'Elroi', value: { id: 1, name: 'Elroi', code: 'Elroi' } },
+      { label: 'Time System', value: { id: 2, name: 'Time System', code: 'TS' } },
+      { label: 'LifeSpeed', value: { id: 3, name: 'LifeSpeed', code: 'LS' } },
+      { label: 'LS2', value: { id: 4, name: 'LS2', code: 'LS2' } },
+      { label: 'PMS', value: { id: 5, name: 'PMS', code: 'PMS' } }
+    ];
+    this.selectedProject = this.projects.filter(m => m.label === 'Elroi')[0].value;
+  }
+
+  projectChange() {
+
   }
 
   navigateToPage(srcFrom: string) {
